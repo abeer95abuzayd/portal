35910deaf6b2daf34dda633668c6de2d3a5ed532
Abeer Abu Zayd <abeer.abuzayd@inspirejo.com>
2023-02-20 11:05:55 +0300

    test suite

---
+++ Test Suites/abeer.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>abeer</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>03e90a1a-c7ab-481d-9a2f-5efa24dd1b6d</testSuiteGuid>
+   <testCaseLink>
+      <guid>bed836b1-7191-4b3e-b4c1-4a2275bd0399</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/login</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
